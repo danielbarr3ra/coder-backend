21cb32955e654bd8a5ceb47a3b1ee4307d1c7f4f
danielbarr3ra <daniel.er.barrera@gmail.com>
2022-07-23 22:12:27 -0500

    getAll

--- Homeworks/Container.js
+++ Homeworks/Container.js
@@ -3,33 +3,45 @@ const fs = require('fs')
 class Container {
     constructor(file) {
         this.filePath = file
-        this.total = 0;
+        this.total = 0
     }
 
-    save(object) {
+    async save(object) {
         const newObject = {
             id: this.total++,
             ...object
         }
         try {
-            fs.promises.appendFile(this.filePath, JSON.stringify(newObject, null, 2))
+            const writting = fs.promises.appendFile(this.filePath, JSON.stringify(newObject, null, 2) + ',\n')
+            await writting
         } catch (error) {
             console.log('could not save the object:')
             console.log(error)
         }
         return newObject.id
     }
-
-
+    async getAll() {
+        const content = fs.promises.readFile(this.filePath, 'utf-8').then((result) => {
+            console.log('here are the objects')
+            return result
+        }).catch((error) => {
+            console.log('could not get the objects')
+            console.log(error)
+        })
+        return content
+    }
 }
 
 const test = new Container('./containerTest.txt')
 
 testObject = {
-    name: 'test1',
-    property: 'prop1'
+    title: 'namae',
+    price: 1234,
+    thumbnail: 'www.something.com'
 }
 test.save(testObject)
 test.save(testObject)
+test.save(testObject)
 
-console.log(test.save(testObject))
+test.save(testObject).then((result) => { console.log(result) })
+test.getAll().then((result) => { console.log(result) })
